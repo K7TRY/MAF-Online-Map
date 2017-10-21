52eec195f00c439f068078d6c843054259f2eddb
Robert Whiting <rswhiting@gmail.com>
2017-10-20 20:46:02 -0600

    added sample data and json versions of csv

---
+++ scripts/csvToJson.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+def csvToJson(file):
+    out = []
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            out.append(row)
+    return json.dumps(out)
+
+def writeToFile(data, filename):
+    file = open(filename, "w")
+    file.write(data)
+    file.close()
+
+if __name__ == "__main__":
+    filename = '../MAF_Served_Airstrips'
+    data = csvToJson(filename + ".csv")
+    writeToFile(data, filename + ".json")
