0d6158508204709b6ea1862b7505a786d0f16902
Robert Whiting <rswhiting@gmail.com>
2017-10-21 11:46:40 -0600

    remove vor and invalid locations from combined data, bad locations piped to bad_data.json for review

--- scripts/combineFiles.py
+++ scripts/combineFiles.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+badData = []
+
 def combineFiles(files):
     data = []
     for file in files:
@@ -9,36 +11,61 @@ def combineFiles(files):
     return data
 
 def cleanData(data):
+    # clean it up
     for key, value in data.items():
         if value == "NULL":
             data[key] = ""
-    # more cleaning needs done? put it here
+    data = cleanupUnits(data)
+    # is it bad data? put it somewhere else
+    if isVOR(data) or isBadLocation(data):
+        return None
+    return data
+
+def isBadLocation(data):
+    lon = float(data["LongDeg"])
+    lat = float(data["LatDeg"])
+    if (lat == 0 and lon  == 0) or (lat > 90 or lat < -90) or (lon > 180 or lon < -180):
+        data["Bad"] = "Invalid lat/long"
+        badData.append(data)
+        return True
+    return False
+
+def isVOR(data):
+    if "VOR" in data["Name"]:
+        data["Bad"] = "VOR"
+        badData.append(data)
+        return True
+    return False
+
+def cleanupUnits(data):
     data["Elevation"] = stripUnits(data.pop("Elevation"))
     data["Slope"] = stripUnits(data.pop("Slope"))
     data["RwDirection"] = data.pop("RwDirection")
     data["RwLength"] = stripUnits(data.pop("RwLength"))
     data["RwWidth"] = stripUnits(data.pop("RwWidth"))
+    # round to 4 decimal places
+    data["LongDeg"] = str(round(float(data["LongDeg"]), 4))
+    data["LatDeg"] = str(round(float(data["LatDeg"]), 4))
     return data
-
+    
 def stripUnits(data):
     return re.sub("[^0-9]","", data).strip()
 
-def writeToFile(data, filename):
+def writeToFile(data, filename, indent=False):
     file = open(filename, "w")
-    file.write(json.dumps(data))
+    if indent:
+        file.write(json.dumps(data, indent=4))
+    else:
+        file.write(json.dumps(data))
     file.close()
 
 def generateCombinedData(filenames):
     data = combineFiles(filenames)
     data = [cleanData(airstrip) for airstrip in data]
+    writeToFile(badData, "../data/bad_data.json", True)
     writeToFile(data, "../data/combined_maf_data.json")
 
 if __name__ == "__main__":
     #filenames = ['../data/papua_airstrips.json']
     filenames = ['../data/Kalimantan Airstrips.json', '../data/MAF_Served_Airstrips.json', '../data/papua_airstrips.json']
     generateCombinedData(filenames)
-
-
-
-#TODO filter out VORs
-#TODO filter out lat/long 0,0
