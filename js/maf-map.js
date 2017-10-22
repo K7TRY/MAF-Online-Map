6218fc1afe288f3d86b7b1ce43785e450912c670
Edwin Steele <edwin-lists@wordspeak.org>
2017-10-22 19:32:26 +1100

    Show runway length and MAF bases

--- js/maf-map.js
+++ js/maf-map.js
@@ -1,25 +1,48 @@
-var geojsonMarkerOptions = {
-	    radius: 8,
-	    fillColor: "#ff7800",
-	    color: "#000",
-	    weight: 1,
-	    opacity: 1,
-	    fillOpacity: 0.8
-};
-
-function popUp(feature, layer) {
-	layer.bindPopup(feature.properties.Name);
-}
-
 var map = L.map('map', {
-	center: [0, -78],
-	zoom: 5
+    center: [0, 0],
+    zoom: 3
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 //var geojsonLayer = new L.GeoJSON.AJAX("/data/MAF_Served_Airstrips.geojson",
-var geojsonLayer = new L.GeoJSON.AJAX("/data/combined_maf_data.geojson",
-	{onEachFeature:popUp}
-);  
+var geojsonLayer = new L.GeoJSON.AJAX("/data/combined_maf_data.geojson", {
+    onEachFeature: function popUp(feature, layer) {
+            var popup_msg = feature.properties.Name;
+            if (feature.properties.RwLength < 500) {
+                popup_msg = feature.properties.Name + " (short runway)";
+            }
+            layer.bindPopup(popup_msg);
+    },
+    pointToLayer: function(feature, latlng) {
+        if (feature.properties.MAFBase == "1") {
+            var maf_icon = L.icon({
+                iconUrl: "/images/airport.png",
+                iconSize:     [20, 20],
+                iconAnchor:   [10, 10],
+            })
+            return L.marker(latlng, {icon: maf_icon});
+        }
+        if (feature.properties.RwLength < 500) {
+            return L.circleMarker(latlng, {
+                "color": "#ff0000",
+                "fillcolor": "#ff0000",
+                "radius": 5,
+                "weight": 1,
+                "opacity": 1,
+                "fillOpacity": 0.8
+            });
+        }
+        else {
+            return L.circleMarker(latlng, {
+                    "color": "#0000ff",
+                    "fillcolor": "#0000ff",
+                    "radius": 5,
+                    "weight": 1,
+                    "opacity": 1,
+                    "fillOpacity": 0.8
+            });
+       }
+    }
+});  
 geojsonLayer.addTo(map)
